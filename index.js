e76f29d9a6da2823b1ce2ec705ed6282e47abf7e
Gia Edgington <gia.edgington@gmail.com>
2019-05-14 16:11:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function takeANumber(lineOfPeople, name){
-  var num = lineOfPeople.push(name);
+function takeANumber(katzDelizLine, name){
+  var num = katzDelizLine.push(name);
   return "Welcome, " + name + ". You are number " + num + " in line."
+}
+
+function nowServing(katzDelizLine){
+  if(katzDelizLine.length == 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    let currentPerson = katzDelizLine.name[0]
+    return currentPerson
+  } 
 }
