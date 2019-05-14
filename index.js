bbde1bb67023920ee2d79f12ca4a92af3e71b815
Gia Edgington <gia.edgington@gmail.com>
2019-05-14 15:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(lineOfPeople, name){
   
-  var num = lineOfPeople.length;
+  var num = lineOfPeople.length + 1;
 
   return "Welcome, " + name + ". You are number " + num + " in line."
 }
